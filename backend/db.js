d3a2ec082c130df5991158cc78c7e8c9479f6430
iamakhileshmishra <akhileshprince1485@gmail.com>
2023-06-21 15:46:16 +0530

    Version 1.0.0

---
+++ backend/db.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoURI =
+  process.env.MONGO_URI; // Customer change url to your db you created in atlas
+// mongoose.set("debug", true);
+
+module.exports = function (callback) {
+  mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(async () => {
+      console.log("Connected to MongoDB");
+
+      const foodCollection = await mongoose.connection.db.collection(
+        "food_items"
+      );
+      const categoryCollection = await mongoose.connection.db.collection(
+        "foodCategory"
+      );
+
+      const foodItems = await foodCollection.find({}).toArray();
+      const categories = await categoryCollection.find({}).toArray();
+
+      // console.log("Food Items:", foodItems);
+      // console.log("Categories:", categories);
+
+      callback(null, foodItems, categories);
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+      callback(err);
+    });
+};
